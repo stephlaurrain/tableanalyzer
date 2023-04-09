5a7e2465194d0bf8dd316d21bb2e3c4f8c4e1853
stephane.laurrain <stephane.laurrain@jiliti-group.com>
2023-04-10 00:49:47 +0200

    into subdir

---
+++ src/frontend/src/sav/router-subdir.js
@@ -0,0 +1,26 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import App from "./App";
+import Mainpage from "./pages/Main/Main";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Mainpage />,
+      },
+      /* {
+        path: "/*",
+        element: <ErrorPage />,
+      }*/,
+      { path: "/404", element: <ErrorPage /> },
+      { path: "*", element: <Navigate to="/404" replace />}
+    ],
+  },
+],  {
+    basename: "/tableanalyze",
+  });
